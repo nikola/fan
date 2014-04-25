fe1f279ad419d3217aa89699529d31e064bb51d6
Nikola Klaric <nikola@klaric.org>
2014-04-26 00:05:42 +0200

    Write certificate in ADS when possible.

--- backend/server/control.py
+++ backend/server/control.py
@@ -1,31 +1,40 @@
 # coding: utf-8
 """
 """
-__author__ = "Nikola Klaric (nikola@klaric.org)"
+__author__ = "Nikola Klaric (nikola@generic.company)"
 __copyright__ = "Copyright (c) 2013-2014 Nikola Klaric"
 
 import os
 import socket
 import bz2
 import base64
-import ctypes
+from uuid import uuid4
+import win32file
 from multiprocessing import Process
 from os import fdopen
 from tempfile import mkstemp
 
 from pants.web import Application
-from pants.http import HTTPServer
+from pants.http import HTTPServer # , WebSocket
 from pants import Engine
 
-from config import DEBUG, ENFORCED_CIPHERS, CERTIFICATE
+from utils.win32 import isNtfsFilesystem
+from config import DEBUG, ENFORCED_CIPHERS, CERTIFICATE, PROJECT_PATH
 from server.routes import module as appRoutes
 
+"""
+class EchoSocket(WebSocket):
+    def on_read(self, data):
+        self.write(data)
+"""
 
 def _startHttpServer(userAgent, port, certificateFile):
-    """
-    """
+
     def _verifyUserAgent(request):
         if DEBUG or (request.is_secure and request.protocol == 'HTTP/1.1' and request.headers.get('Accept-Language', None) == 'en-us,en' and request.headers.get('User-Agent', None) == userAgent):
+            # if request.path.endswith('.socket'):
+            #     EchoSocket(request)
+            # else:
             app(request)
         else:
             request.finish()
@@ -40,8 +49,6 @@ def _startHttpServer(userAgent, port, certificateFile):
 
 
 def _getVacantPort():
-    """
-    """
     s = socket.socket()
     s.bind(("", 0))
     port = s.getsockname()[1]
@@ -50,18 +57,26 @@ def _getVacantPort():
 
 
 def _getCertificateLocation():
-    """
-    """
     certificate = bz2.decompress(base64.decodestring(CERTIFICATE))
 
-    fd, pathname = mkstemp(suffix=".tmp", prefix="ASPNETSetup_")
-    if not DEBUG:
-        # FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_TEMPORARY | FILE_ATTRIBUTE_NOT_CONTENT_INDEXED
-        ctypes.windll.kernel32.SetFileAttributesW(unicode(pathname), 2 | 4 | 256 | 8192)
+    if isNtfsFilesystem():
+        executable = os.path.join(PROJECT_PATH, 'backend', 'run.py')
+        pathname = '%s:%s' % (executable, uuid4().hex)
+        # if win32file.GetFileAttributesW(unicode(executable)) & 1:
+        #     win32file.SetFileAttributesW(unicode(executable), 0)
+        with open(pathname, 'wb') as fp:
+            fp.write(certificate)
+        # win32file.SetFileAttributesW(unicode(executable), 1) # FILE_ATTRIBUTE_READONLY
+    else:
+        fd, pathname = mkstemp(suffix='.tmp', prefix='ASPNETSetup_')
+        if not DEBUG:
+            # FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_TEMPORARY | FILE_ATTRIBUTE_NOT_CONTENT_INDEXED
+            # ctypes.windll.kernel32.SetFileAttributesW(unicode(pathname), 2 | 4 | 256 | 8192)
+            win32file.SetFileAttributesW(unicode(pathname), 2 | 4 | 256 | 8192)
 
-    fp = fdopen(fd, "w")
-    fp.write(certificate)
-    fp.close()
+        fp = fdopen(fd, 'w')
+        fp.write(certificate)
+        fp.close()
 
     return pathname
 

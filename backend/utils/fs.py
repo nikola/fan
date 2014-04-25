0cd571812274758ee1d3c018ef5bcec3282023b4
Nikola Klaric <nikola@klaric.org>
2014-04-26 01:32:05 +0200

    Added file stream enumeration.

---
+++ backend/utils/fs.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+"""
+"""
+__author__ = 'Nikola Klaric (nikola@generic.company)'
+__copyright__ = 'Copyright (c) 2014 Nikola Klaric'
+
+from win32file import FindStreams
+
+
+def getFileStreams(pathname):
+    return [str(name[1:name.rindex(':')]) for length, name in FindStreams(unicode(pathname))[1:]]
+
+
+def removeFileStreams(pathname):
+    pass

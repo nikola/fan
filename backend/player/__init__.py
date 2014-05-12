9cfd49bbd7d0804e661d64830aaac51739f9d2cb
Nikola Klaric <nikola@klaric.org>
2014-05-12 19:44:17 +0200

    Experimental changes.

--- backend/player/__init__.py
+++ backend/player/__init__.py
@@ -5,7 +5,7 @@
     http://www.emailarchitect.net/easendmail/sdk/html/object_reg_a.htm
     http://www.s-code.com/kayako/index.php?_m=knowledgebase&_a=viewarticle&kbarticleid=64
 
-
+    DSVidRen=12 -> madVR
 """
 __author__ = "Nikola Klaric (nikola@generic.company)"
 __copyright__ = "Copyright (c) 2013-2014 Nikola Klaric"
@@ -18,6 +18,7 @@ import win32api
 from utils.win32 import getAppStoragePathname
 # from updater.const import *
 from updater.lib import *
+from settings.player import MPCHC_INI
 
 PLAYER_AMALGAM_PATH = os.path.join(getAppStoragePathname(), 'amalgam')
 
@@ -26,32 +27,50 @@ MT_PATCH = """
         <comClass clsid="{E1A8B82A-32CE-4B0D-BE0D-AA68C772E423}" threadingModel="Both" description="madVR"></comClass>
         <comClass clsid="{F352C9C1-D39D-4622-A279-978A60927CDE}" threadingModel="Both" description="madVR Property Page"></comClass>
     </file>
+    <file name="LAVSplitter.ax">
+        <comClass clsid="{171252A0-8820-4AFE-9DF8-5C92B2D66B04}" threadingModel="Both" description="LAV Splitter"></comClass>
+        <comClass clsid="{A19DE2F2-2F74-4927-8436-61129D26C141}" threadingModel="Both" description="LAV Splitter Properties"></comClass>
+        <comClass clsid="{B98D13E7-55DB-4385-A33D-09FD1BA26338}" threadingModel="Both" description="LAV Splitter Source"></comClass>
+        <comClass clsid="{56904B22-091C-4459-A2E6-B1F4F946B55F}" threadingModel="Both" description="LAV Splitter Input Formats"></comClass>
+    </file>
+    <file name="LAVAudio.ax">
+        <comClass clsid="{E8E73B6B-4CB3-44A4-BE99-4F7BCB96E491}" threadingModel="Both" description="LAV Audio Decoder"></comClass>
+        <comClass clsid="{2D8F1801-A70D-48F4-B76B-7F5AE022AB54}" threadingModel="Both" description="LAV Audio Properties"></comClass>
+        <comClass clsid="{BD72668E-6BFF-4CD1-8480-D465708B336B}" threadingModel="Both" description="LAV Audio Format Settings"></comClass>
+        <comClass clsid="{20ED4A03-6AFD-4FD9-980B-2F6143AA0892}" threadingModel="Both" description="LAV Audio Status"></comClass>
+        <comClass clsid="{C89FC33C-E60A-4C97-BEF4-ACC5762B6404}" threadingModel="Both" description="LAV Audio Mixer"></comClass>
+    </file>
+    <file name="LAVVideo.ax">
+        <comClass clsid="{EE30215D-164F-4A92-A4EB-9D4C13390F9F}" threadingModel="Both" description="LAV Video Decoder"></comClass>
+        <comClass clsid="{278407C2-558C-4BED-83A0-B6FA454200BD}" threadingModel="Both" description="LAV Video Properties"></comClass>
+        <comClass clsid="{2D4D6F88-8B41-40A2-B297-3D722816648B}" threadingModel="Both" description="LAV Video Format Settings"></comClass>
+    </file>
 """
 
 
-def updateComponents():
-    def log(text, color=BLACK):
+def _updateComponents():
+    def _log(text, color=BLACK):
         #  windll.Kernel32.SetConsoleTextAttribute(CONSOLE_HANDLER, color)
         sys.stdout.write(text)
 
-    setLogger(log)
+    setLogger(_log)
 
     components = [
-        ('MPC-HC',
+        ('MPC-HC', 'mpc-hc.exe',
             Component(r'MPC-HC\MPC-HC',
                 getLatestReleaseVersion = mpcHc_getLatestReleaseVersion,
                 getInstalledVersion = mpcHc_getInstalledVersion,
                 installLatestReleaseVersion = mpcHc_installLatestReleaseVersion,
             )
         ),
-        ('LAV Filters',
+        ('LAV Filters', 'LAVSplitter.ax',
             Component(LAVFILTERS_CLSID,
                 getLatestReleaseVersion = lavFilters_getLatestReleaseVersion,
                 getInstalledVersion = lavFilters_getInstalledVersion,
                 installLatestReleaseVersion = lavFilters_installLatestReleaseVersion,
             )
         ),
-        ('madVR',
+        ('madVR', 'madVR.ax',
             Component(MADVR_CLSID,
                 getLatestReleaseVersion = madVr_getLatestReleaseVersion,
                 getInstalledVersion = madVr_getInstalledVersion,
@@ -60,51 +79,61 @@ def updateComponents():
         ),
     ]
 
-    for name, instance in components:
-        log('\n')
+    mpcHcVersionSnapshot = components[0][2].getInstalledVersion(os.path.join(PLAYER_AMALGAM_PATH, 'mpc-hc.exe'))[0]
+
+    for name, filename, instance in components:
+        _log('\n')
+
+        pathname = os.path.join(PLAYER_AMALGAM_PATH, filename)
 
         latestVersion = getattr(instance, 'getLatestReleaseVersion')()
         if latestVersion is None:
-            log('ERROR: Could not retrieve version info of the latest %s release.\n' % name, RED)
+            _log('ERROR: Could not retrieve version info of the latest %s release.\n' % name, RED)
         else:
-            log('Latest release version of %s: %s\n' % (name, latestVersion))
+            _log('Latest release version of %s: %s\n' % (name, latestVersion))
 
-            installedVersion, detectedInstallationPath = instance.getInstalledVersion(PLAYER_AMALGAM_PATH)
+            installedVersion, detectedInstallationPath = instance.getInstalledVersion(pathname)
             mustInstall = False
             if installedVersion is not None:
-                log('Installed version: %s\n\t%s\n' % (installedVersion, detectedInstallationPath))
+                _log('Installed version: %s\n\t%s\n' % (installedVersion, detectedInstallationPath))
 
                 if not installedVersion or getVersionTuple(installedVersion) < getVersionTuple(latestVersion):
                     mustInstall = True
                 else:
-                    log('%s does not need to be updated.\n' % name, GREEN)
+                    _log('%s does not need to be updated.\n' % name, GREEN)
             else:
-                log('%s does not seem to be installed on the local machine.\n' % name)
+                _log('%s does not seem to be installed on the local machine.\n' % name)
                 mustInstall = True
 
             if mustInstall:
-                try:
-                    getattr(instance, 'installLatestReleaseVersion')(latestVersion, PLAYER_AMALGAM_PATH, False, True, True)
-                except Exception, e:
-                    log(' ERROR: %s\n' % e.message, RED)
+                # try:
+                getattr(instance, 'installLatestReleaseVersion')(latestVersion, PLAYER_AMALGAM_PATH, silent=False, archive=True, compact=True, compatText=True)
+                # except Exception, e:
+                #     log(' ERROR: %s\n' % e.message, RED)
+                # else:
+                currentInstalledVersion, currentInstallationPath = instance.getInstalledVersion(pathname)
+                # print
+                # print currentInstalledVersion
+                # print currentInstallationPath
+                if currentInstallationPath is None or getVersionTuple(currentInstalledVersion) != getVersionTuple(latestVersion):
+                    _log('\nFailed to %s %s %s.\n'
+                        % ('update to' if installedVersion is not None else 'install', name, latestVersion), RED)
                 else:
-                    currentInstalledVersion, currentInstallationPath = instance.getInstalledVersion(PLAYER_AMALGAM_PATH)
-                    if currentInstallationPath is None or getVersionTuple(currentInstalledVersion) != getVersionTuple(latestVersion):
-                        log('\nFailed to %s %s %s.\n'
-                            % ('update to' if installedVersion is not None else 'install', name, latestVersion), RED)
-                    else:
-                        log(' done.\n')
-                        if detectedInstallationPath != currentInstallationPath:
-                            log('%s %s is now installed in:\n\t%s\n'
-                                % (name, latestVersion, currentInstallationPath))
-                            if installedVersion is not None:
-                                log('Your previous installation of %s %s remains in:\n\t%s\n'
-                                    % (name, installedVersion, detectedInstallationPath))
-                        log('Successfully %s %s. No errors.\n'
-                            % ('updated' if installedVersion is not None else 'installed', name), GREEN)
+                    _log(' done.\n')
+                    if detectedInstallationPath != currentInstallationPath:
+                        _log('%s %s is now installed in:\n\t%s\n'
+                            % (name, latestVersion, currentInstallationPath))
+                        if installedVersion is not None:
+                            _log('Your previous installation of %s %s remains in:\n\t%s\n'
+                                % (name, installedVersion, detectedInstallationPath))
+                    _log('Successfully %s %s. No errors.\n'
+                        % ('updated' if installedVersion is not None else 'installed', name), GREEN)
+
+    mpcHcVersionCurrent = components[0][2].getInstalledVersion(os.path.join(PLAYER_AMALGAM_PATH, 'mpc-hc.exe'))[0]
+    return mpcHcVersionSnapshot is None or mpcHcVersionSnapshot != mpcHcVersionCurrent
 
 
-def patchMpcHcManifest():
+def _patchManifest():
     location = os.path.join(PLAYER_AMALGAM_PATH, 'mpc-hc.exe')
 
     handle = win32api.LoadLibraryEx(location, None, 0x20)
@@ -127,3 +156,14 @@ def patchMpcHcManifest():
             # Touch file.
             with open(location, 'a'):
                 os.utime(location, None)
+
+
+def _writeConfig():
+    with open(os.path.join(PLAYER_AMALGAM_PATH, 'mpc-hc.ini'), 'wb') as fp:
+        fp.write(MPCHC_INI)
+
+
+def update():
+    if _updateComponents():
+        _patchManifest()
+        _writeConfig()
--- backend/presenter/control.py
+++ backend/presenter/control.py
@@ -9,16 +9,31 @@ import traceback
 import codecs
 import uuid
 import imp
+from multiprocessing import Process, Queue
 
 import win32gui
 import win32api
 import win32con
 
+"""
+from ctypes import *
+class COLORREF(Structure):
+    _fields_ = [
+    ("byRed", c_byte),
+    ("byGreen", c_byte),
+    ("byBlue", c_byte)
+    ]
+print windll.gdi32.CreateSolidBrush(COLORREF(255, 0, 0))
+"""
+
 from config import *
 from utils.win32 import getNormalizedPathname
 
 # from chromium.hooks import ClientHandler
+# if globals().has_key('cefpython'):
+#     print "before", cefpython
 
+# print "after", cefpython
 
 def handleException(excType, excValue, traceObject):
     """
@@ -37,14 +52,68 @@ def handleException(excType, excValue, traceObject):
     errorMsg = errorMsg.encode("ascii", errors="replace").decode("ascii", errors="replace")
     print("\n%s\n" % errorMsg)
 
-    stopChromeContainer()
+    _destroyChromeContainer()
     os._exit(1)
 
 
-def launchChrome(agent, url, callbacks):
-    """
-    """
-    global cefpython
+def startPresenter(agent, url, callbacks):
+    global globalPresenterProcess
+    # global cefpython
+    global onCloseCallbacks
+
+    global className, windowName
+
+    onCloseCallbacks = callbacks
+
+    className = 'kaboom_%s' % uuid.uuid4().hex
+    windowName = 'ka-BOOM'
+
+    cefpython = imp.load_dynamic("cefpython_py27", os.path.join(PROJECT_PATH, "backend", "presenter", "cef", "libgfx.dll"))
+
+    # cefpython = imp.load_dynamic("cefpython_py27", os.path.join(PROJECT_PATH, "backend", "presenter", "cef", "libgfx.dll"))
+
+    print '0className', className
+    print '0windowName', windowName
+
+    globalPresenterProcess = Process(target=_createChromeContainer, args=(agent, url, className, windowName)) # , callbacks))
+    globalPresenterProcess.start()
+
+
+def stopPresenter():
+    global globalPresenterProcess
+    # cefpython,
+    global onCloseCallbacks
+
+    for callback in onCloseCallbacks: callback()
+
+    global className, windowName
+
+    print '1className', className
+    print '1windowName', windowName
+    windowHandle = win32gui.FindWindow(className, windowName)
+
+    print 'windowHandle', windowHandle
+
+    print 'GetBrowserByWindowHandle', cefpython.GetBrowserByWindowHandle(windowHandle)
+
+    browser = cefpython.GetBrowserByWindowHandle(windowHandle)
+    browser.CloseBrowser()
+
+
+    cefpython.QuitMessageLoop()
+    cefpython.Shutdown()
+
+    # cefpython.browser.CloseBrowser()
+
+    # QuitApplication()
+
+    print 2
+
+    globalPresenterProcess.terminate()
+
+
+def _createChromeContainer(agent, url, className, windowName): # , callbacks):
+    # global cefpython
 
     # TODO: Refactor:
     #   https://github.com/kivy-garden/garden.cefpython/blob/master/__init__.py
@@ -52,26 +121,27 @@ def launchChrome(agent, url, callbacks):
     # Implant into .EXE:
     #   http://www.pyinstaller.org/export/v2.0/project/doc/Manual.html#collect
 
-    cefpython = imp.load_dynamic("cefpython_py27", os.path.join(PROJECT_PATH, "backend", "presenter", "cef", "libgfx.dll"))
+    # cefpython = imp.load_dynamic("cefpython_py27", os.path.join(PROJECT_PATH, "backend", "presenter", "cef", "libgfx.dll"))
 
     # Memorize callback functions that must be executed before shutting down the CEF container.
-    global onCloseCallbacks
-    onCloseCallbacks = callbacks
+    # global onCloseCallbacks
+    # onCloseCallbacks = callbacks
 
     cefpython.g_debug = DEBUG
     cefpython.g_debugFile = getNormalizedPathname("debug.log")
 
     cefpython.Initialize(
         dict(   # applicationSettings
-            debug                     = DEBUG,
-            cache_path                = '',
-            log_file                  = getNormalizedPathname("debug.log"),
-            log_severity              = cefpython.LOGSEVERITY_INFO,
-            release_dcheck_enabled    = DEBUG, # Disable in production
-            browser_subprocess_path   = "%s\\%s" % (cefpython.GetModuleDirectory(), "iexplore"),
-            user_agent                = agent,
-            ignore_certificate_errors = True,
-            remote_debugging_port     = 8090,
+            debug                       = DEBUG,
+            cache_path                  = '',
+            log_file                    = getNormalizedPathname("debug.log"),
+            log_severity                = cefpython.LOGSEVERITY_INFO,
+            release_dcheck_enabled      = DEBUG, # Disable in production
+            browser_subprocess_path     = "%s\\%s" % (cefpython.GetModuleDirectory(), "iexplore"),
+            user_agent                  = agent,
+            ignore_certificate_errors   = True,
+            remote_debugging_port       = 8090,
+            multi_threaded_message_loop = False,
         ),
         {
             # https://code.google.com/p/chromiumembedded/source/browse/trunk/cef3/libcef/common/cef_switches.cc
@@ -109,45 +179,23 @@ def launchChrome(agent, url, callbacks):
         },
     )
 
+    """
     windowId = createChromeWindow(
-        title = "ka-BOOM",
-        className = "kaboom_%s" % uuid.uuid4().hex,
+        className = className, # 'kaboom_%s' % uuid.uuid4().hex,
+        windowName = windowName, # 'ka-BOOM',
         iconPathname = getNormalizedPathname("../presenter/cef/icon.ico"),
     )
-
-    windowInfo = cefpython.WindowInfo()
-    windowInfo.SetAsChild(windowId)
-
-    cefpython.CreateBrowserSync(
-        windowInfo,
-        CHROME_BROWSER_SETTINGS,
-        navigateUrl=url,
-    )
-
-    # clientHandler = ClientHandler()
-    # browser.SetClientHandler(clientHandler)
-
-    cefpython.MessageLoop()
-    cefpython.Shutdown()
+    """
 
 
-def stopChromeContainer():
-    global cefpython, onCloseCallbacks
+    iconPathname = getNormalizedPathname("../presenter/cef/icon.ico")
 
-    for callback in onCloseCallbacks: callback()
-
-    cefpython.QuitMessageLoop()
-    cefpython.Shutdown()
-
-
-def createChromeWindow(title, className, iconPathname):
-    global cefpython
 
     wndclass = win32gui.WNDCLASS()
     wndclass.hInstance = win32api.GetModuleHandle(None)
     wndclass.lpszClassName = className
-    wndclass.style = win32con.CS_VREDRAW | win32con.CS_HREDRAW
-    wndclass.hbrBackground = win32con.NULL_BRUSH
+    wndclass.style = 0 # win32con.CS_NOCLOSE # win32con.CS_VREDRAW | win32con.CS_HREDRAW
+    wndclass.hbrBackground = win32gui.GetStockObject(win32con.WHITE_BRUSH)
     wndclass.hCursor = win32gui.LoadCursor(0, win32con.IDC_ARROW)
     wndclass.lpfnWndProc = {
         win32con.WM_CLOSE: CloseWindow,
@@ -164,17 +212,25 @@ def createChromeWindow(title, className, iconPathname):
     # SetWindowPos function:
     #   http://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
 
-    if DEBUG:
-        dwExStyle = win32con.WS_EX_APPWINDOW
-        style = win32con.WS_OVERLAPPEDWINDOW | win32con.WS_CLIPCHILDREN | win32con.WS_VISIBLE
-    else:
-        dwExStyle = win32con.WS_EX_APPWINDOW | win32con.WS_EX_TOPMOST | win32con.WS_EX_LAYERED
-        style = win32con.WS_POPUP | win32con.WS_VISIBLE | win32con.WS_SYSMENU
+    # if DEBUG:
+    #     dwExStyle = win32con.WS_EX_APPWINDOW
+    #     style = win32con.WS_OVERLAPPEDWINDOW | win32con.WS_CLIPCHILDREN | win32con.WS_VISIBLE
+    # else:
 
-    windowID = win32gui.CreateWindowEx(
+    # BUG: only works when /HKEY_CURRENT_USER/Software/Microsoft/Windows/DWM/Composition = 0
+
+
+
+
+
+    dwExStyle = win32con.WS_EX_APPWINDOW | win32con.WS_EX_TOPMOST | win32con.WS_EX_LAYERED | win32con.WS_EX_TRANSPARENT
+    # dwExStyle = win32con.WS_EX_LAYERED
+    style = win32con.WS_VISIBLE | win32con.WS_POPUP | win32con.WS_CLIPCHILDREN | win32con.WS_CLIPSIBLINGS
+
+    windowId = win32gui.CreateWindowEx(
         dwExStyle,
         className,
-        title,
+        windowName,
         style,
         win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT,
         win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1),
@@ -184,15 +240,154 @@ def createChromeWindow(title, className, iconPathname):
         None, # reserved
     )
 
-    if not DEBUG:
-        win32gui.SetLayeredWindowAttributes(windowID, win32api.RGB(255, 255, 255), 0, win32con.LWA_COLORKEY)
+    #  if not DEBUG:
+    win32gui.SetLayeredWindowAttributes(windowId, win32api.RGB(255, 255, 255), 0, win32con.LWA_COLORKEY)
+    # win32gui.SetLayeredWindowAttributes(windowID, win32api.RGB(0, 0, 0), 255, win32con.LWA_COLORKEY)
+
+    # win32gui.ShowWindow(windowID, win32con.SW_SHOWNORMAL)
+
+    # win32gui.SetWindowPos(windowID, None, 0, 0, 1920, 1080, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_NOZORDER | win32con.SWP_FRAMECHANGED)
+    # SetWindowPos(window_->GetNativeWindow(), NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
 
     # To turn off:
     # win32gui.SetWindowLong(windowID, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(windowID, win32con.GWL_EXSTYLE) & ~win32con.WS_EX_LAYERED)
     # win32gui.RedrawWindow(windowID, None, None, win32con.RDW_ERASE | win32con.RDW_INVALIDATE | win32con.RDW_FRAME | win32con.RDW_ALLCHILDREN)
 
     # 100% Opaque:
-    # win32gui.SetLayeredWindowAttributes(windowID, 0, (255 * 100) / 100, win32con.LWA_ALPHA)
+    # win32gui.SetLayeredWindowAttributes(windowID, 0, (20 * 100) / 100, win32con.LWA_ALPHA)
+
+    bigX = win32api.GetSystemMetrics(win32con.SM_CXICON)
+    bigY = win32api.GetSystemMetrics(win32con.SM_CYICON)
+    bigIcon = win32gui.LoadImage(0, iconPathname, win32con.IMAGE_ICON, bigX, bigY, win32con.LR_LOADFROMFILE)
+    win32api.SendMessage(windowId, win32con.WM_SETICON, win32con.ICON_BIG, bigIcon)
+
+    smallX = win32api.GetSystemMetrics(win32con.SM_CXSMICON)
+    smallY = win32api.GetSystemMetrics(win32con.SM_CYSMICON)
+    smallIcon = win32gui.LoadImage(0, iconPathname, win32con.IMAGE_ICON, smallX, smallY, win32con.LR_LOADFROMFILE)
+    win32api.SendMessage(windowId, win32con.WM_SETICON, win32con.ICON_SMALL, smallIcon)
+
+    # return windowID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    print 'windowId', windowId
+
+    windowInfo = cefpython.WindowInfo()
+    windowInfo.SetAsChild(windowId)
+
+    browser = cefpython.CreateBrowserSync(
+        windowInfo,
+        CHROME_BROWSER_SETTINGS,
+        navigateUrl=url,
+    )
+
+    # print 0
+    # print browser
+
+    # clientHandler = ClientHandler()
+    # browser.SetClientHandler(clientHandler)
+
+    cefpython.MessageLoop()
+    # cefpython.Shutdown()
+
+
+def _destroyChromeContainer():
+    # global cefpython, \
+    global onCloseCallbacks
+
+    for callback in onCloseCallbacks: callback()
+
+    cefpython.QuitMessageLoop()
+    cefpython.Shutdown()
+
+
+"""
+def createChromeWindow(className, windowName, iconPathname):
+    # global cefpython
+
+    print '2className', className
+    print '2windowName', windowName
+
+    wndclass = win32gui.WNDCLASS()
+    wndclass.hInstance = win32api.GetModuleHandle(None)
+    wndclass.lpszClassName = className
+    wndclass.style = 0 # win32con.CS_NOCLOSE # win32con.CS_VREDRAW | win32con.CS_HREDRAW
+    wndclass.hbrBackground = win32gui.GetStockObject(win32con.WHITE_BRUSH)
+    wndclass.hCursor = win32gui.LoadCursor(0, win32con.IDC_ARROW)
+    wndclass.lpfnWndProc = {
+        win32con.WM_CLOSE: CloseWindow,
+        win32con.WM_DESTROY: QuitApplication,
+        win32con.WM_SIZE: cefpython.WindowUtils.OnSize,
+        win32con.WM_SETFOCUS: cefpython.WindowUtils.OnSetFocus,
+        win32con.WM_ERASEBKGND: cefpython.WindowUtils.OnEraseBackground
+    }
+    win32gui.RegisterClass(wndclass)
+
+    # Extended Window Styles:
+    #   http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+
+    # SetWindowPos function:
+    #   http://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
+
+    # if DEBUG:
+    #     dwExStyle = win32con.WS_EX_APPWINDOW
+    #     style = win32con.WS_OVERLAPPEDWINDOW | win32con.WS_CLIPCHILDREN | win32con.WS_VISIBLE
+    # else:
+
+    # BUG: only works when /HKEY_CURRENT_USER/Software/Microsoft/Windows/DWM/Composition = 0
+
+
+
+
+
+    dwExStyle = win32con.WS_EX_APPWINDOW | win32con.WS_EX_TOPMOST | win32con.WS_EX_LAYERED | win32con.WS_EX_TRANSPARENT
+    # dwExStyle = win32con.WS_EX_LAYERED
+    style = win32con.WS_VISIBLE | win32con.WS_POPUP | win32con.WS_CLIPCHILDREN | win32con.WS_CLIPSIBLINGS
+
+    windowID = win32gui.CreateWindowEx(
+        dwExStyle,
+        className,
+        windowName,
+        style,
+        win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT,
+        win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1),
+        0, # parent
+        0, # menu
+        wndclass.hInstance,
+        None, # reserved
+    )
+
+    #  if not DEBUG:
+    win32gui.SetLayeredWindowAttributes(windowID, win32api.RGB(255, 255, 255), 0, win32con.LWA_COLORKEY)
+    # win32gui.SetLayeredWindowAttributes(windowID, win32api.RGB(0, 0, 0), 255, win32con.LWA_COLORKEY)
+
+    # win32gui.ShowWindow(windowID, win32con.SW_SHOWNORMAL)
+
+    # win32gui.SetWindowPos(windowID, None, 0, 0, 1920, 1080, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_NOZORDER | win32con.SWP_FRAMECHANGED)
+    # SetWindowPos(window_->GetNativeWindow(), NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+    # To turn off:
+    # win32gui.SetWindowLong(windowID, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(windowID, win32con.GWL_EXSTYLE) & ~win32con.WS_EX_LAYERED)
+    # win32gui.RedrawWindow(windowID, None, None, win32con.RDW_ERASE | win32con.RDW_INVALIDATE | win32con.RDW_FRAME | win32con.RDW_ALLCHILDREN)
+
+    # 100% Opaque:
+    # win32gui.SetLayeredWindowAttributes(windowID, 0, (20 * 100) / 100, win32con.LWA_ALPHA)
 
     bigX = win32api.GetSystemMetrics(win32con.SM_CXICON)
     bigY = win32api.GetSystemMetrics(win32con.SM_CYICON)
@@ -205,19 +400,52 @@ def createChromeWindow(title, className, iconPathname):
     win32api.SendMessage(windowID, win32con.WM_SETICON, win32con.ICON_SMALL, smallIcon)
 
     return windowID
-
+"""
 
 def CloseWindow(windowHandle, message, wparam, lparam):
-    global cefpython, onCloseCallbacks
+    # global cefpython, \
+    global onCloseCallbacks
+
+    print 'close'
+    print windowHandle
+    print 'end'
 
     browser = cefpython.GetBrowserByWindowHandle(windowHandle)
     browser.CloseBrowser()
 
-    for callback in onCloseCallbacks: callback()
+
+    # for callback in onCloseCallbacks: callback()
 
     return win32gui.DefWindowProc(windowHandle, message, wparam, lparam)
 
 
-def QuitApplication(*args, **kwargs):
+def QuitApplication(windowHandle, *args, **kwargs): # windowHandle, message, wparam, lparam):
+    # win32gui.UnregisterClass(parent.className, None)
+
+    """
+	browser = cefpython.GetBrowserByWindowID(windowHandle)
+	browser.CloseBrowser()
+
+
+	cefwindow.DestroyWindow(windowHandle)
+
+        ---->
+
+            win32gui.DestroyWindow(windowHandle)
+            #className = GetWindowClassName(windowHandle)
+            #win32gui.UnregisterClass(className, None)
+            #del g_windows[windowID] # Let window with this className be created again.
+
+
+	win32gui.PostQuitMessage(0)
+    """
+
     win32gui.PostQuitMessage(0)
     return 0
+
+
+if __name__ == '__main__':
+    print '--------- MAIN ------------'
+#
+#     print cefpython
+#     # cefpython.t = 1

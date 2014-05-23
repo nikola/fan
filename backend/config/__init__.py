a01edc7a49cacef481906deab57c643ea6e432a9
Nikola Klaric <nikola@klaric.org>
2014-05-23 16:49:44 +0200

    Updated JS libs.

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -14,15 +14,15 @@ APP_VENDOR = 'Generic Company'
 PROJECT_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
 
 RESOURCES_SCRIPT = [
-
-    # "vendor/lodash/lodash.min.js",
+    'vendor/lodash/lodash.min.js',
+    'vendor/bacon/bacon.min.js',
+    'vendor/cortex/cortex.min.js',
     'vendor/jquery/jquery.min.js',
-    'app/js/app.js',
     'app/js/lib/sockets.js',
+    'app/js/app.js',
 ]
 
-
 RESOURCES_STYLE = [
-    "app/css/app.css",
-    "app/css/animations.css",
+    'app/css/app.css',
+    'app/css/animations.css',
 ]

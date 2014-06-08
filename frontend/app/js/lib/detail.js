533947aa69dd9ffd34c772cbeacefa885204fd6e
Nikola Klaric <nikola@klaric.org>
2014-06-08 22:01:51 +0200

    Changed CSS of movie detail page.

--- frontend/app/js/lib/detail.js
+++ frontend/app/js/lib/detail.js
@@ -10,7 +10,7 @@ if (!('lib' in ka)) ka.lib = {};
 
 ka.lib.updateDetailPage = function () {
     var movie = ka.state.gridLookupMatrix[ka.config.gridMaxRows * ka.state.gridPage + ka.state.gridFocusY][ka.state.gridFocusX];
-    $('#boom-movie-detail-title').text(movie.titleOriginal + ' (' + movie.releaseYear + ')');
+    $('#boom-movie-detail-title').text(movie.titleOriginal); //  + ' (' + movie.releaseYear + ')');
     $('#boom-movie-detail-description').text(movie.overview);
     $('#boom-movie-detail-poster img').attr('src', '');
     $('#boom-movie-detail').css('backgroundImage', 'none');

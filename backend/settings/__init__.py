5fb0255ccef296aae25c5dd71fc86d76dc561d4b
Nikola Klaric <nikola@klaric.org>
2014-06-15 03:33:17 +0200

    Added constant for app storage pathname.

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -4,4 +4,8 @@
 __author__ = 'Nikola Klaric (nikola@generic.company)'
 __copyright__ = 'Copyright (c) 2013-2014 Nikola Klaric'
 
+from utils.win32 import getAppStoragePathname
+
 DEBUG = True
+
+APP_STORAGE_PATH = getAppStoragePathname()

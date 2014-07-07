3d6e948b003da15a88931ce44d8cd40c25ebe044
Nikola Klaric <nikola@klaric.org>
2014-07-07 23:48:13 +0200

    Added default log file formatting.

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -9,3 +9,9 @@ from utils.win32 import getAppStoragePathname
 DEBUG = True
 
 APP_STORAGE_PATH = getAppStoragePathname()
+LOG_CONFIG = dict(
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M',
+    level=20, # logging.INFO
+    filemode='a',
+)

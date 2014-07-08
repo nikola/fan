e9387d3df9e5e8851edbd1fce802c9dcc6c90439
Nikola Klaric <nikola@klaric.org>
2014-07-08 21:35:50 +0200

    Reorganized velocity resources.

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -12,16 +12,19 @@ from cert import *
 PROJECT_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
 
 RESOURCES_SCRIPT = [
-    'vendor/lodash/lodash.min.js',
-    'vendor/bacon/bacon.min.js',
+    # 'vendor/lodash/lodash.min.js',
+    # 'vendor/bacon/bacon.min.js',
     'vendor/cortex/cortex.min.js',
     'vendor/jquery/jquery.min.js',
     # 'vendor/jquery/jquery.swipe-events.js',
-    'vendor/jquery/jquery.velocity.min.js',
+
     'vendor/jquery/jquery.waitforimages.min.js',
     'vendor/misc/color-thief.min.js',
     'vendor/misc/keypress.min.js',
+
+    'vendor/velocity/velocity.min.js',
     'vendor/velocity/velocity.ui.js',
+
     'app/js/lib/sockets.js',
     'app/js/lib/colors.js',
     'app/js/lib/l10n.js',

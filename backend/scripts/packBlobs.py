6cb6972172ca568c781bc724dbfd5204328ebf13
Nikola Klaric <nikola@klaric.org>
2014-07-10 18:02:12 +0200

    Compress fonts, too. Changed root directory.

--- backend/scripts/packBlobs.py
+++ backend/scripts/packBlobs.py
@@ -7,49 +7,56 @@ __copyright__ = 'Copyright (c) 2013-2014 Nikola Klaric'
 import os.path
 import re
 import pylzma
-# TODO: pack fonts, too !!!
+from hashlib import md5 as MD5
 
 
-RESOURCES_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'frontend')
-BLOBS_PATH = os.path.join(RESOURCES_PATH, '..', 'backend', 'blobs')
+RESOURCES_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..') # , 'frontend')
+BLOBS_PATH = os.path.join(RESOURCES_PATH, 'backend', 'blobs')
 
 
 RESOURCES_SCRIPT = [
     # 'vendor/lodash/lodash.min.js',
     # 'vendor/bacon/bacon.min.js',
-    'vendor/cortex/cortex.min.js',
-    'vendor/jquery/jquery.min.js',
+    'frontend/vendor/cortex/cortex.min.js',
+    'frontend/vendor/jquery/jquery.min.js',
     # 'vendor/jquery/jquery.swipe-events.js',
 
     # 'vendor/jquery/jquery.waitforimages.min.js',
-    'vendor/misc/color-thief.min.js',
-    'vendor/misc/keypress.min.js',
+    'frontend/vendor/misc/color-thief.min.js',
+    'frontend/vendor/misc/keypress.min.js',
 
-    'vendor/velocity/velocity.min.js',
-    'vendor/velocity/velocity.ui.js',
+    'frontend/vendor/velocity/velocity.min.js',
+    'frontend/vendor/velocity/velocity.ui.js',
 
-    'app/js/lib/sockets.js',
-    'app/js/lib/colors.js',
-    'app/js/lib/l10n.js',
-    'app/js/lib/receiver.js',
-    'app/js/lib/hotkeys.js',
-    'app/js/lib/lang.js',
-    'app/js/lib/config.js',
-    'app/js/lib/grid.js',
-    'app/js/lib/detail.js',
-    'app/js/app.js',
+    'frontend/app/js/lib/sockets.js',
+    'frontend/app/js/lib/colors.js',
+    'frontend/app/js/lib/l10n.js',
+    'frontend/app/js/lib/receiver.js',
+    'frontend/app/js/lib/hotkeys.js',
+    'frontend/app/js/lib/lang.js',
+    'frontend/app/js/lib/config.js',
+    'frontend/app/js/lib/grid.js',
+    'frontend/app/js/lib/detail.js',
+    'frontend/app/js/app.js',
 ]
 
 RESOURCES_STYLE = [
-    'app/css/fonts.css',
-    'app/css/icons.css',
+    'frontend/app/css/fonts.css',
+    'frontend/app/css/icons.css',
 
-    'app/css/app.css',
+    'frontend/app/css/app.css',
 
-    'app/css/buttons.css',
-    'app/css/config.css',
-    'app/css/grid.css',
-    'app/css/detail.css',
+    'frontend/app/css/buttons.css',
+    'frontend/app/css/config.css',
+    'frontend/app/css/grid.css',
+    'frontend/app/css/detail.css',
+]
+
+RESOURCES_FONT = [
+    'frontend/fonts/regular.ttf',
+    'frontend/fonts/bold.ttf',
+    'frontend/fonts/italic.ttf',
+    'frontend/fonts/icons.ttf',
 ]
 
 
@@ -76,14 +83,14 @@ def _readStrip(filename, delimiters='{}'):
 
 def run():
     # Compress bootloader.
-    pathname = os.path.join(RESOURCES_PATH, 'app', 'html', 'boot.html')
+    pathname = os.path.join(RESOURCES_PATH, 'frontend', 'app', 'html', 'boot.html')
     html = _readStrip(pathname, '{};')
     compressed = pylzma.compress(html)
     with open(os.path.join(BLOBS_PATH, 'b1932b8b02de45bc9ec66ebf1c75bb15'), 'wb') as fp:
         fp.write(compressed)
 
     # Compress GUI.
-    pathname = os.path.join(RESOURCES_PATH, 'app', 'html', 'gui.html')
+    pathname = os.path.join(RESOURCES_PATH, 'frontend', 'app', 'html', 'gui.html')
     with open(pathname, 'rb') as fp:
         html = fp.read()
     html = re.sub(r'>\s*<', '><', html)
@@ -105,6 +112,20 @@ def run():
     compressed = pylzma.compress(html)
     with open(os.path.join(BLOBS_PATH, 'c9d25707d3a84c4d80fdb6b0789bdcf6'), 'wb') as fp:
         fp.write(compressed)
+
+    # Compress fonts.
+    for pathname in RESOURCES_FONT:
+        with open(os.path.join(RESOURCES_PATH, pathname), 'rb') as fp:
+            ttf = fp.read()
+        compressed = pylzma.compress(ttf)
+
+        identifier = os.path.basename(pathname).replace('.ttf', '')
+        md5 = MD5()
+        md5.update(identifier)
+        filename = md5.hexdigest()
+
+        with open(os.path.join(BLOBS_PATH, filename), 'wb') as fp:
+            fp.write(compressed)
 
 
 if __name__ == '__main__':

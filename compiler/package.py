2ec6da230c82993dabdd55c1a1541324d390cffb
Nikola Klaric <nikola@klaric.org>
2014-07-25 00:51:45 +0200

    Updated packager script.

--- compiler/package.py
+++ compiler/package.py
@@ -32,52 +32,67 @@ def run():
             if parent.startswith('\\'):
                 parent = parent[1:]
 
-            targetDirectory = os.path.join(TARGET_CLEAN_DIR, parent)
-            try:
-                os.makedirs(targetDirectory)
-            except OSError:
-                pass
-
-            with open(pathname, 'rb') as fp:
-                script = fp.read()
-
-            script = re.compile(r'from settings import DEBUG').sub('', script)
-            script = re.compile(r'if DEBUG:.*?# END if DEBUG', re.M | re.DOTALL).sub('', script)
-            script = re.compile(r'if DEBUG and .*?# END if DEBUG', re.M | re.DOTALL).sub('', script)
-            script = re.compile(r'if DEBUG or ').sub('if ', script)
-            script = re.compile(r'return DEBUG or ').sub('return ', script)
-            script = re.compile(r'(debug=DEBUG)').sub('()', script)
-            script = re.compile(r' and not DEBUG').sub('', script)
-            script = re.compile(r'DEBUG = True').sub('', script)
-
-            script = minification.remove_comments_and_docstrings(script)
-
-            script = script.strip()
-
-            script = re.compile(r"__author__ = '[^']+'").sub('', script)
-            script = re.compile(r"__copyright__ = '[^']+'").sub('', script)
-
-            script = script.strip()
-
-            script = minification.remove_blank_lines(script)
-
-            script = script.strip()
-
-            script = minification.remove_blank_lines(script)
-
-            script = script.strip()
-
-            script = minification.dedent(script, use_tabs=False)
-
-            script = script.strip()
-
-            if parent == 'updater':
-                script = '# coding: iso-8859-1\n' + script
+            if parent == 'scripts':
+                continue
+            elif parent == 'settings' and filename == 'player.py':
+                continue
             else:
-                script = '# coding: utf-8\n' + script
+                targetDirectory = os.path.join(TARGET_CLEAN_DIR, parent)
+                try:
+                    os.makedirs(targetDirectory)
+                except OSError:
+                    pass
 
-            with open(os.path.join(TARGET_CLEAN_DIR, parent, filename), 'wb') as fp:
-                fp.write(script)
+                with open(pathname, 'rb') as fp:
+                    script = fp.read()
+
+                if parent == 'identifier' and filename == '__init__.py':
+                    key = re.search(r"THEMOVIEDB_API_KEY = '([^']+)'", script).groups()[0].encode('rot13')
+                    param = 'api_key'.encode('rot13')
+                    algo = "(str(255-0xe0)+'tor')[::-1]"
+                    script = re.compile(r"'api_key': THEMOVIEDB_API_KEY").sub("'%s'.decode(%s): '%s'.decode(%s)" % (param, algo, key, algo), script)
+                    script = re.compile(r"THEMOVIEDB_API_KEY = '([^']+)'").sub('', script)
+
+                if script.find("'User-Agent': ENTROPY_SEED") != -1:
+                    param = 'User-Agent'.encode('rot13')
+                    algo = "(str(255-0xe0)+'tor')[::-1]"
+                    script = re.compile(r"'User-Agent': ENTROPY_SEED").sub("'%s'.decode(%s): ENTROPY_SEED" % (param, algo), script)
+
+                script = re.compile(r'from settings import DEBUG').sub('', script)
+                script = re.compile(r'if DEBUG:.*?# END if DEBUG', re.M | re.DOTALL).sub('', script)
+                script = re.compile(r'if (True|False): # DEBUG.*?# END if DEBUG', re.M | re.DOTALL).sub('', script)
+                script = re.compile(r'if DEBUG and .*?# END if DEBUG', re.M | re.DOTALL).sub('', script)
+                script = re.compile(r'if DEBUG or ').sub('if ', script)
+                script = re.compile(r'return DEBUG or ').sub('return ', script)
+                script = re.compile(r'(debug=DEBUG)').sub('()', script)
+                script = re.compile(r' and not DEBUG').sub('', script)
+                script = re.compile(r'DEBUG = (True|False)').sub('', script)
+
+                script = minification.remove_comments_and_docstrings(script)
+
+                script = script.strip()
+
+                script = re.compile(r"__author__ = '[^']+'").sub('', script)
+                script = re.compile(r"__copyright__ = '[^']+'").sub('', script)
+
+                script = script.strip()
+
+                script = minification.remove_blank_lines(script)
+
+                script = script.strip()
+
+
+                script = minification.dedent(script, use_tabs=False)
+
+                script = script.strip()
+
+                if parent == 'updater':
+                    script = '# coding: iso-8859-1\n' + script
+                else:
+                    script = '# coding: utf-8\n' + script
+
+                with open(os.path.join(TARGET_CLEAN_DIR, parent, filename), 'wb') as fp:
+                    fp.write(script)
 
 
 if __name__ == '__main__':

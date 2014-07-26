1b940e8f4c3db6bee6f38d3f6246dd4b33433697
Nikola Klaric <nikola@klaric.org>
2014-07-26 18:42:59 +0200

    Set fixed port over non-SSL.

--- backend/boot.py
+++ backend/boot.py
@@ -18,7 +18,7 @@ from settings import LOG_CONFIG
 from models import initialize as initStreamManager
 from utils.system import isCompatiblePlatform, isNtfsFilesystem, getScreenResolution, isDesktopCompositionEnabled
 from utils.agent import getUserAgent
-from utils.net import getVacantPort, getCertificateLocation
+from utils.net import getCertificateLocation
 from utils.fs import getLogFileHandler, createAppStorageStructure
 from utils.config import getCurrentUserConfig
 from orchestrator.control import start as startOrchestrator, stop as stopOrchestrator
@@ -30,38 +30,6 @@ from presenter.control import start as present
 
 if __name__ == '__main__':
     freeze_support()
-
-    logging.basicConfig(**LOG_CONFIG)
-    logger = logging.getLogger('application')
-    logger.addHandler(getLogFileHandler('application'))
-
-    if not isCompatiblePlatform():
-        windll.user32.MessageBoxA(0, 'This application is only compatible with Windows Vista or newer.', 'Error', 0)
-        logger.critical('Aborting because system is not Windows Vista or newer.')
-        sys.exit()
-
-    if not isNtfsFilesystem():
-        windll.user32.MessageBoxA(0, 'This application must be run from an NTFS partition.', 'Error', 0)
-        logger.critical('Aborting because system is not on NTFS partition.')
-        sys.exit()
-
-    if getScreenResolution() != (1920, 1080):
-        windll.user32.MessageBoxA(0, 'This application must be run at 1920x1080 screen resolution.', 'Error', 0)
-        logger.critical('Aborting because screen resolution is not 1920x1080.')
-        sys.exit()
-
-    if not isDesktopCompositionEnabled():
-        windll.user32.MessageBoxA(0, 'This application requires that the Desktop Window Manager (DWM) is enabled.', 'Error', 0)
-        logger.critical('Aborting because DWM is disabled.')
-        sys.exit()
-
-    # Hide unpacked directory.
-    if getattr(sys, 'frozen', None):
-        # FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_NOT_CONTENT_INDEXED
-        win32file.SetFileAttributesW(unicode(sys._MEIPASS), 2 | 4 | 8192)
-
-        # Enable SSL support in requests library when running as EXE.
-        os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(sys._MEIPASS, 'requests', 'cacert.pem')
 
     def _shutdown():
         # Presenter has been closed, now kick off clean-up tasks.
@@ -85,25 +53,58 @@ if __name__ == '__main__':
         os.remove(certificateLocation)
 
         logger.info('Closing application.')
-        logger.info('<' * 80)
+        # logger.info('<' * 80)
         logger.info('')
 
         # Circumvent SystemExit exception handler.
         os._exit(1)
 
-    # sys.excepthook = handleException
-
-    if True: # DEBUG
-        from scripts.packBlobs import run as runPackBlobs
-        runPackBlobs()
-    # END if DEBUG
-
-    logger.info('>' * 80)
-    logger.info('Starting application.')
-
     try:
+        logging.basicConfig(**LOG_CONFIG)
+        logger = logging.getLogger('application')
+        logger.addHandler(getLogFileHandler('application'))
+
+        if not isCompatiblePlatform():
+            windll.user32.MessageBoxA(0, 'This application is only compatible with Windows Vista or newer.', 'Error', 0)
+            logger.critical('Aborting because system is not Windows Vista or newer.')
+            sys.exit()
+
+        if not isNtfsFilesystem():
+            windll.user32.MessageBoxA(0, 'This application must be run from an NTFS partition.', 'Error', 0)
+            logger.critical('Aborting because system is not on NTFS partition.')
+            sys.exit()
+
+        if getScreenResolution() != (1920, 1080):
+            windll.user32.MessageBoxA(0, 'This application must be run at 1920x1080 screen resolution.', 'Error', 0)
+            logger.critical('Aborting because screen resolution is not 1920x1080.')
+            sys.exit()
+
+        if not isDesktopCompositionEnabled():
+            windll.user32.MessageBoxA(0, 'This application requires that the Desktop Window Manager (DWM) is enabled.', 'Error', 0)
+            logger.critical('Aborting because DWM is disabled.')
+            sys.exit()
+
+        # Hide unpacked directory.
+        if getattr(sys, 'frozen', None):
+            # FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_NOT_CONTENT_INDEXED
+            win32file.SetFileAttributesW(unicode(sys._MEIPASS), 2 | 4 | 8192)
+
+            # Enable SSL support in requests library when running as EXE.
+            os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(sys._MEIPASS, 'requests', 'cacert.pem')
+
+        # sys.excepthook = handleException
+
+        if True: # DEBUG
+            from scripts.packBlobs import run as runPackBlobs
+            runPackBlobs()
+        # END if DEBUG
+
+        # logger.info('>' * 80)
+        logger.info('Starting application.')
+
+
         # Create AppData sub-folders.
-        createAppStorageStructure()
+        # createAppStorageStructure()
 
         # Create DB connection here to initialize models.
         initStreamManager()
@@ -126,7 +127,7 @@ if __name__ == '__main__':
         analyzer = startAnalyzer(interProcessQueue)
         player = startPlayer(interProcessQueue)
 
-        arguments = (getUserAgent(), serverPort, uuid4().hex, uuid4().hex, True, userConfig)
+        arguments = (getUserAgent(), serverPort, uuid4().hex, uuid4().hex, False, userConfig)
 
         # Start process, but spawn file scanner and watcher only after receiving a kick-off event from the presenter.
         orchestrator = startOrchestrator(interProcessQueue, certificateLocation, *arguments)

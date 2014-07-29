1f4e2a0e04aea49387155a29bd6a445250b02b18
Nikola Klaric <nikola@klaric.org>
2014-07-30 01:39:54 +0200

    Ensure clean slate.

--- compiler/build.bat
+++ compiler/build.bat
@@ -1,4 +1,5 @@
 REM signtool signwizard
 REM http://timestamp.verisign.com/scripts/timstamp.dll
 REM http://nikola.github.io/ka-BOOM/
+del dist\ka-BOOM.exe
 pyi-build ka-BOOM.spec --distpath=dist --workpath=build --noconfirm --ascii

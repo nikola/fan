5dc0b09d519bf7d125a666d36a943eb831115359
Nikola Klaric <nikola@klaric.org>
2014-07-31 02:48:12 +0200

    Corrected handling of non-Latin-1 movie titles.

--- backend/identifier/fixture.py
+++ backend/identifier/fixture.py
@@ -30,503 +30,503 @@ TRAILERS_HD = {
 
 TOP_250 = [
     # 1
-    ("The Shawshank Redemption", 1994),                                 
+    (u"The Shawshank Redemption", 1994),                                 
     # 2
-    ("The Godfather", 1972),
+    (u"The Godfather", 1972),
     #
-    ("The Godfather: Part II", 1974),
+    (u"The Godfather: Part II", 1974),
     #
-    ("The Dark Knight", 2008),
+    (u"The Dark Knight", 2008),
     #
-    ("Pulp Fiction", 1994),
+    (u"Pulp Fiction", 1994),
     #
-    ("Il buono, il brutto, il cattivo", 1966),
+    (u"Il buono, il brutto, il cattivo", 1966),
     #
-    ("Schindler's List", 1993),
+    (u"Schindler's List", 1993),
     #
-    ("12 Angry Men", 1957),
+    (u"12 Angry Men", 1957),
     #
-    ("The Lord of the Rings: The Return of the King", 2003),
+    (u"The Lord of the Rings: The Return of the King", 2003),
     #
-    ("Fight Club", 1999),
+    (u"Fight Club", 1999),
     #
-    ("The Lord of the Rings: The Fellowship of the Ring", 2001),
+    (u"The Lord of the Rings: The Fellowship of the Ring", 2001),
     #
-    ("Star Wars: Episode V - The Empire Strikes Back", 1980),
+    (u"Star Wars: Episode V - The Empire Strikes Back", 1980),
     #
-    ("Inception", 2010),
+    (u"Inception", 2010),
     #
-    ("Forrest Gump", 1994),
+    (u"Forrest Gump", 1994),
     #
-    ("One Flew Over the Cuckoo's Nest", 1975),
+    (u"One Flew Over the Cuckoo's Nest", 1975),
     #
-    ("The Lord of the Rings: The Two Towers", 2002),
+    (u"The Lord of the Rings: The Two Towers", 2002),
     #
-    ("Goodfellas", 1990),
+    (u"Goodfellas", 1990),
     #
-    ("Star Wars", 1977),
+    (u"Star Wars: Episode IV - A New Hope", 1977),
     #
-    ("The Matrix", 1999),
+    (u"The Matrix", 1999),
     #
-    ("Shichinin no samurai", 1954),
+    (u"Shichinin no samurai", 1954),
     #
-    ("Cidade de Deus", 2002),
+    (u"Cidade de Deus", 2002),
     #
-    ("Se7en", 1995),
+    (u"Se7en", 1995),
     #
-    ("The Usual Suspects", 1994),
+    (u"The Usual Suspects", 1995),
     #
-    ("The Silence of the Lambs", 2000),
+    (u"The Silence of the Lambs", 1991),
     #
-    ("C'era una volta il West", 1968),
+    (u"C'era una volta il West", 1968),
     #
-    ("It's a Wonderful Life", 1946),
+    (u"It's a Wonderful Life", 1946),
     #
-    ("Léon", 1994),
+    (u"Léon", 1994),
     #
-    ("Casablanca", 1942),
+    (u"Casablanca", 1942),
     #
-    ("La vita è bella", 1997),
+    (u"La vita è bella", 1997),
     #
-    ("Raiders of the Lost Ark", 1981),
+    (u"Raiders of the Lost Ark", 1981),
     #
-    ("Psycho", 1960),
+    (u"Psycho", 1960),
     #
-    ("American History X", 1998),
+    (u"American History X", 1998),
     #
-    ("Rear Window", 1954),
+    (u"Rear Window", 1954),
     #
-    ("City Lights", 1931),
+    (u"City Lights", 1931),
     #
-    ("Saving Private Ryan", 1998),
+    (u"Saving Private Ryan", 1998),
     #
-    ("Sen to Chihiro no kamikakushi", 2001),
+    (u"Spirited Away", 2001),
     #
-    ("Intouchables", 2011),
+    (u"Intouchables", 2011),
     #
-    ("Memento", 2000),
+    (u"Memento", 2000),
     #
-    ("Modern Times", 1936),
+    (u"Modern Times", 1936),
     #
-    ("Terminator 2: Judgment Day", 1991),
+    (u"Terminator 2: Judgment Day", 1991),
     #
-    ("Sunset Blvd.", 1950),
+    (u"Sunset Blvd.", 1950),
     #
-    ("The Pianist", 2002),
+    (u"The Pianist", 2002),
     #
-    ("Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb", 1964),
+    (u"Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb", 1964),
     #
-    ("Apocalypse Now", 1979),
+    (u"Apocalypse Now", 1979),
     #
-    ("The Green Mile", 1999),
+    (u"The Green Mile", 1999),
     #
-    ("The Departed", 2006),
+    (u"The Departed", 2006),
     #
-    ("Gladiator", 2000),
+    (u"Gladiator", 2000),
     #
-    ("Back to the Future", 1985),
+    (u"Back to the Future", 1985),
     #
-    ("Alien", 1979),
+    (u"Alien", 1979),
     #
-    ("The Dark Knight Rises", 2012),
+    (u"The Dark Knight Rises", 2012),
     #
-    ("Django Unchained", 2012),
+    (u"Django Unchained", 2012),
     #
-    ("The Prestige", 2006),
+    (u"The Prestige", 2006),
     #
-    ("Das Leben der Anderen", 2006),
+    (u"Das Leben der Anderen", 2006),
     #
-    ("The Great Dictator", 1940),
+    (u"The Great Dictator", 1940),
     #
-    ("The Shining", 1980),
+    (u"The Shining", 1980),
     #
-    ("Nuovo Cinema Paradiso", 1988),
+    (u"Nuovo Cinema Paradiso", 1988),
     #
-    ("The Lion King", 1994),
+    (u"The Lion King", 1994),
     #
-    ("Paths of Glory", 1957),
+    (u"Paths of Glory", 1957),
     #
-    ("American Beauty", 1999),
+    (u"American Beauty", 1999),
     #
-    ("WALL·E", 2008),
+    (u"WALL·E", 2008),
     #
-    ("North by Northwest", 1959),
+    (u"North by Northwest", 1959),
     #
-    ("Le fabuleux destin d'Amélie Poulain", 2001),
+    (u"Le fabuleux destin d'Amélie Poulain", 2001),
     #
-    ("Citizen Kane", 1941),
+    (u"Citizen Kane", 1941),
     #
-    ("Aliens", 1986),
+    (u"Aliens", 1986),
     #
-    ("Vertigo", 1958),
+    (u"Vertigo", 1958),
     #
-    ("Toy Story 3", 2010),
+    (u"Toy Story 3", 2010),
     #
-    ("M", 1931),
+    (u"M", 1931),
     #
-    ("Das Boot", 1981),
+    (u"Das Boot", 1981),
     #
-    ("A Clockwork Orange", 1971),
+    (u"A Clockwork Orange", 1971),
     #
-    ("Taxi Driver", 1976),
+    (u"Taxi Driver", 1976),
     #
-    ("Oldboy", 2003),
+    (u"Oldboy", 2003),
     #
-    ("Double Indemnity", 1944),
+    (u"Double Indemnity", 1944),
     #
-    ("Mononoke-hime", 1997),
+    (u"Princess Mononoke", 1997),
     #
-    ("Reservoir Dogs", 1992),
+    (u"Reservoir Dogs", 1992),
     #
-    ("Star Wars: Episode VI - Return of the Jedi", 1983),
+    (u"Star Wars: Episode VI - Return of the Jedi", 1983),
     #
-    ("Once Upon a Time in America", 1984),
+    (u"Once Upon a Time in America", 1984),
     #
-    ("To Kill a Mockingbird", 1962),
+    (u"To Kill a Mockingbird", 1962),
     #
-    ("Requiem for a Dream", 2000),
+    (u"Requiem for a Dream", 2000),
     #
-    ("Braveheart", 1995),
+    (u"Braveheart", 1995),
     #
-    ("Lawrence of Arabia", 1962),
+    (u"Lawrence of Arabia", 1962),
     #
-    ("Hotaru no haka", 1988),
+    (u"Hotaru no haka", 1988),
     #
-    ("Eternal Sunshine of the Spotless Mind", 2004),
+    (u"Eternal Sunshine of the Spotless Mind", 2004),
     #
-    ("Witness for the Prosecution", 1958),
+    (u"Witness for the Prosecution", 1958),
     #
-    ("Full Metal Jacket", 1987),
+    (u"Full Metal Jacket", 1987),
     #
-    ("Singin' in the Rain", 1952),
+    (u"Singin' in the Rain", 1952),
     #
-    ("The Sting", 1973),
+    (u"The Sting", 1973),
     #
-    ("Ladri di biciclette", 1948),
+    (u"Ladri di biciclette", 1948),
     #
-    ("Monty Python and the Holy Grail", 1974),
+    (u"Monty Python and the Holy Grail", 1975),
     #
-    ("Amadeus", 1984),
+    (u"Amadeus", 1984),
     #
-    ("All About Eve", 1950),
+    (u"All About Eve", 1950),
     #
-    ("X-Men: Days of Future Past", 2014),
+    (u"X-Men: Days of Future Past", 2014),
     #
-    ("Snatch", 2000),
+    (u"Snatch", 2000),
     #
-    ("Rashômon", 1950),
+    (u"Rashômon", 1950),
     #
-    ("L.A. Confidential", 1997),
+    (u"L.A. Confidential", 1997),
     #
-    ("The Treasure of the Sierra Madre", 1948),
+    (u"The Treasure of the Sierra Madre", 1948),
     #
-    ("The Apartment", 1960),
+    (u"The Apartment", 1960),
     #
-    ("Some Like It Hot", 1959),
+    (u"Some Like It Hot", 1959),
     #
-    ("The Third Man", 1949),
+    (u"The Third Man", 1949),
     #
-    ("Per qualche dollaro in più", 1965),
+    (u"Per qualche dollaro in più", 1965),
     #
-    ("Indiana Jones and the Last Crusade", 1989),
+    (u"Indiana Jones and the Last Crusade", 1989),
     #
-    ("Inglourious Basterds", 2009),
+    (u"Inglourious Basterds", 2009),
     #
-    ("Jodaeiye Nader az Simin", 2011),
+    (u"A Separation", 2011),
     #
-    ("The Kid", 1921),
+    (u"The Kid", 1921),
     #
-    ("2001: A Space Odyssey", 1968),
+    (u"2001: A Space Odyssey", 1968),
     #
-    ("Batman Begins", 2005),
+    (u"Batman Begins", 2005),
     #
-    ("Yôjinbô", 1961),
+    (u"Yôjinbô", 1961),
     #
-    ("Unforgiven", 1992),
+    (u"Unforgiven", 1992),
     #
-    ("Metropolis", 1927),
+    (u"Metropolis", 1927),
     #
-    ("Raging Bull", 1980),
+    (u"Raging Bull", 1980),
     #
-    ("Toy Story", 1995),
+    (u"Toy Story", 1995),
     #
-    ("Chinatown", 1974),
+    (u"Chinatown", 1974),
     #
-    ("Scarface", 1983),
+    (u"Scarface", 1983),
     #
-    ("The Wolf of Wall Street", 2013),
+    (u"The Wolf of Wall Street", 2013),
     #
-    ("Up", 2009),
+    (u"Up", 2009),
     #
-    ("Die Hard", 1988),
+    (u"Die Hard", 1988),
     #
-    ("Der Untergang", 2004),
+    (u"Der Untergang", 2004),
     #
-    ("The Great Escape", 1963),
+    (u"The Great Escape", 1963),
     #
-    ("Mr. Smith Goes to Washington", 1939),
+    (u"Mr. Smith Goes to Washington", 1939),
     #
-    ("El laberinto del fauno", 2006),
+    (u"El laberinto del fauno", 2006),
     #
-    ("Taare Zameen Par", 2007),
+    (u"Taare Zameen Par", 2007),
     #
-    ("On the Waterfront", 1954),
+    (u"On the Waterfront", 1954),
     #
-    ("The Bridge on the River Kwai", 1957),
+    (u"The Bridge on the River Kwai", 1957),
     #
-    ("Heat", 1995),
+    (u"Heat", 1995),
     #
-    ("Jagten", 2013),
+    (u"Jagten", 2013),
     #
-    ("3 Idiots", 2009),
+    (u"3 Idiots", 2009),
     #
-    ("Det sjunde inseglet", 1957),
+    (u"Det sjunde inseglet", 1957),
     #
-    ("Smultronstället", 1957),
+    (u"Smultronstället", 1957),
     #
-    ("Ikiru", 1952),
+    (u"Ikiru", 1952),
     #
-    ("The Elephant Man", 1980),
+    (u"The Elephant Man", 1980),
     #
-    ("Ran", 1985),
+    (u"Ran", 1985),
     #
-    ("Tonari no Totoro", 1988),
+    (u"Tonari no Totoro", 1988),
     #
-    ("The General", 1926),
+    (u"The General", 1926),
     #
-    ("Blade Runner", 1982),
+    (u"Blade Runner", 1982),
     #
-    ("The Gold Rush", 1925),
+    (u"The Gold Rush", 1925),
     #
-    ("The Grand Budapest Hotel", 2014),
+    (u"The Grand Budapest Hotel", 2014),
     #
-    ("Lock, Stock and Two Smoking Barrels", 1998),
+    (u"Lock, Stock and Two Smoking Barrels", 1998),
     #
-    ("Good Will Hunting", 1997),
+    (u"Good Will Hunting", 1997),
     #
-    ("Gran Torino", 2008),
+    (u"Gran Torino", 2008),
     #
-    ("Rebecca", 1940),
+    (u"Rebecca", 1940),
     #
-    ("El Secreto de sus Ojos", 2009),
+    (u"El Secreto de sus Ojos", 2009),
     #
-    ("The Big Lebowski", 1998),
+    (u"The Big Lebowski", 1998),
     #
-    ("Casino", 1995),
+    (u"Casino", 1995),
     #
-    ("It Happened One Night", 1934),
+    (u"It Happened One Night", 1934),
     #
-    ("Warrior", 2011),
+    (u"Warrior", 2011),
     #
-    ("Rush", 2013),
+    (u"Rush", 2013),
     #
-    ("V for Vendetta", 2006),
+    (u"V for Vendetta", 2006),
     #
-    ("The Deer Hunter", 1978),
+    (u"The Deer Hunter", 1978),
     #
-    ("Cool Hand Luke", 1967),
+    (u"Cool Hand Luke", 1967),
     #
-    ("12 Years a Slave", 2013),
+    (u"12 Years a Slave", 2013),
     #
-    ("Fargo", 1996),
+    (u"Fargo", 1996),
     #
-    ("The Maltese Falcon", 1941),
+    (u"The Maltese Falcon", 1941),
     #
-    ("Hauru no ugoku shiro", 2004),
+    (u"Hauru no ugoku shiro", 2004),
     #
-    ("Rang De Basanti", 2006),
+    (u"Rang De Basanti", 2006),
     #
-    ("Trainspotting", 1996),
+    (u"Trainspotting", 1996),
     #
-    ("Gone with the Wind", 1939),
+    (u"Gone with the Wind", 1939),
     #
-    ("Into the Wild", 2007),
+    (u"Into the Wild", 2007),
     #
-    ("How to Train Your Dragon", 2010),
+    (u"How to Train Your Dragon", 2010),
     #
-    ("Hotel Rwanda", 2004),
+    (u"Hotel Rwanda", 2004),
     #
-    ("Judgment at Nuremberg", 1961),
+    (u"Judgment at Nuremberg", 1961),
     #
-    ("The Sixth Sense", 1999),
+    (u"The Sixth Sense", 1999),
     #
-    ("Butch Cassidy and the Sundance Kid", 1969),
+    (u"Butch Cassidy and the Sundance Kid", 1969),
     #
-    ("A Beautiful Mind", 2001),
+    (u"A Beautiful Mind", 2001),
     #
-    ("The Thing", 1982),
+    (u"The Thing", 1982),
     #
-    ("Dial M for Murder", 1954),
+    (u"Dial M for Murder", 1954),
     #
-    ("Le salaire de la peur", 1953),
+    (u"Le salaire de la peur", 1953),
     #
-    ("Platoon", 1986),
+    (u"Platoon", 1986),
     #
-    ("Kill Bill: Vol. 1", 2003),
+    (u"Kill Bill: Vol. 1", 2003),
     #
-    ("No Country for Old Men", 2007),
+    (u"No Country for Old Men", 2007),
     #
-    ("Annie Hall", 1977),
+    (u"Annie Hall", 1977),
     #
-    ("Finding Nemo", 2003),
+    (u"Finding Nemo", 2003),
     #
-    ("Mary and Max", 2009),
+    (u"Mary and Max", 2009),
     #
-    ("Sin City", 2005),
+    (u"Sin City", 2005),
     #
-    ("Life of Brian", 1979),
+    (u"Life of Brian", 1979),
     #
-    ("Touch of Evil", 1958),
+    (u"Touch of Evil", 1958),
     #
-    ("Les Diaboliques", 1955),
+    (u"Les Diaboliques", 1955),
     #
-    ("Network", 1976),
+    (u"Network", 1976),
     #
-    ("The Princess Bride", 1987),
+    (u"The Princess Bride", 1987),
     #
-    ("Stand by Me", 1986),
+    (u"Stand by Me", 1986),
     #
-    ("Amores perros", 2000),
+    (u"Amores perros", 2000),
     #
-    ("The Wizard of Oz", 1939),
+    (u"The Wizard of Oz", 1939),
     #
-    ("Incendies", 2010),
+    (u"Incendies", 2010),
     #
-    ("The Avengers", 2012),
+    (u"The Avengers", 2012),
     #
-    ("Ben-Hur", 1959),
+    (u"Ben-Hur", 1959),
     #
-    ("There Will Be Blood", 2007),
+    (u"There Will Be Blood", 2007),
     #
-    ("Her", 2013),
+    (u"Her", 2013),
     #
-    ("The Grapes of Wrath", 1940),
+    (u"The Grapes of Wrath", 1940),
     #
-    ("Les Quatre Cents Coups", 1959),
+    (u"Les Quatre Cents Coups", 1959),
     #
-    ("The Best Years of Our Lives", 1946),
+    (u"The Best Years of Our Lives", 1946),
     #
-    ("Million Dollar Baby", 2004),
+    (u"Million Dollar Baby", 2004),
     #
-    ("Hachi: A Dog's Tale", 2009),
+    (u"Hachi: A Dog's Tale", 2009),
     #
-    ("In the Name of the Father", 1993),
+    (u"In the Name of the Father", 1993),
     #
-    ("Donnie Darko", 2001),
+    (u"Donnie Darko", 2001),
     #
-    ("Otto e Mezzo", 1963),
+    (u"Otto e Mezzo", 1963),
     #
-    ("The Bourne Ultimatum", 2007),
+    (u"The Bourne Ultimatum", 2007),
     #
-    ("Strangers on a Train", 1951),
+    (u"Strangers on a Train", 1951),
     #
-    ("Persona", 1966),
+    (u"Persona", 1966),
     #
-    ("Gandhi", 1982),
+    (u"Gandhi", 1982),
     #
-    ("High Noon", 1952),
+    (u"High Noon", 1952),
     #
-    ("The King's Speech", 2010),
+    (u"The King's Speech", 2010),
     #
-    ("Notorious", 1946),
+    (u"Notorious", 1946),
     #
-    ("Jaws", 1975),
+    (u"Jaws", 1975),
     #
-    ("Kaze no tani no Naushika", 1984),
+    (u"Kaze no tani no Naushika", 1984),
     #
-    ("Infernal Affairs", 2002),
+    (u"Infernal Affairs", 2002),
     #
-    ("Twelve Monkeys", 1995),
+    (u"Twelve Monkeys", 1995),
     #
-    ("Fanny och Alexander", 1982),
+    (u"Fanny och Alexander", 1982),
     #
-    ("The Terminator", 1984),
+    (u"The Terminator", 1984),
     #
-    ("Ip Man", 2008),
+    (u"Ip Man", 2008),
     #
-    ("La strada", 1954),
+    (u"La strada", 1954),
     #
-    ("Stalker", 1979),
+    (u"Stalker", 1979),
     #
-    ("Lagaan: Once Upon a Time in India", 2001),
+    (u"Lagaan: Once Upon a Time in India", 2001),
     #
-    ("The Night of the Hunter", 1955),
+    (u"The Night of the Hunter", 1955),
     #
-    ("Groundhog Day", 1993),
+    (u"Groundhog Day", 1993),
     #
-    ("Harry Potter and the Deathly Hallows: Part 2", 2011),
+    (u"Harry Potter and the Deathly Hallows: Part 2", 2011),
     #
-    ("Rocky", 1976),
+    (u"Rocky", 1976),
     #
-    ("Dog Day Afternoon", 1975),
+    (u"Dog Day Afternoon", 1975),
     #
-    ("Shutter Island", 2010),
+    (u"Shutter Island", 2010),
     #
-    ("Captain America: The Winter Soldier", 2014),
+    (u"Captain America: The Winter Soldier", 2014),
     #
-    ("Who's Afraid of Virginia Woolf?", 1966),
+    (u"Who's Afraid of Virginia Woolf?", 1966),
     #
-    ("La haine", 1995),
+    (u"La haine", 1995),
     #
-    ("The Big Sleep", 1946),
+    (u"The Big Sleep", 1946),
     #
-    ("La battaglia di Algeri", 1965),
+    (u"La battaglia di Algeri", 1966),
     #
-    ("Pirates of the Caribbean: The Curse of the Black Pearl", 2003),
+    (u"Pirates of the Caribbean: The Curse of the Black Pearl", 2003),
     #
-    ("Before Sunrise", 1995),
+    (u"Before Sunrise", 1995),
     #
-    ("Barry Lyndon", 1975),
+    (u"Barry Lyndon", 1975),
     #
-    ("Monsters, Inc.", 2001),
+    (u"Monsters, Inc.", 2001),
     #
-    ("Gravity", 2013),
+    (u"Gravity", 2013),
     #
-    ("The Graduate", 1967),
+    (u"The Graduate", 1967),
     #
-    ("Memories of Murder", 2003),
+    (u"Memories of Murder", 2003),
     #
-    ("Roman Holiday", 1953),
+    (u"Roman Holiday", 1953),
     #
-    ("The Hustler", 1961),
+    (u"The Hustler", 1961),
     #
-    ("Castle in the Sky", 1986),
+    (u"Castle in the Sky", 1986),
     #
-    ("Festen", 1998),
+    (u"Festen", 1998),
     #
-    ("Per un pugno di dollari", 1964),
+    (u"Per un pugno di dollari", 1964),
     #
-    ("The Help", 2011),
+    (u"The Help", 2011),
     #
-    ("In the Mood for Love", 2000),
+    (u"In the Mood for Love", 2000),
     #
-    ("A Christmas Story", 1983),
+    (u"A Christmas Story", 1983),
     #
-    ("Stalag 17", 1953),
+    (u"Stalag 17", 1953),
     #
-    ("Slumdog Millionaire", 2008),
+    (u"Slumdog Millionaire", 2008),
     #
-    ("The Truman Show", 1998),
+    (u"The Truman Show", 1998),
     #
-    ("Swades", 2004),
+    (u"Swades", 2004),
     #
-    ("Underground", 1995),
+    (u"Underground", 1995),
     #
-    ("Rope", 1948),
+    (u"Rope", 1948),
     #
-    ("The Killing", 1956),
+    (u"The Killing", 1956),
     #
-    ("Tropa de Elite 2", 2010),
+    (u"Tropa de Elite 2", 2010),
     #
-    ("Beauty and the Beast", 1991),
+    (u"Beauty and the Beast", 1991),
     #
-    ("Jurassic Park", 1993),
+    (u"Jurassic Park", 1993),
     #
-    ("Black Swan", 2010),
+    (u"Black Swan", 2010),
     #
-    ("Trois couleurs : Rouge", 1994),
+    (u"Trois couleurs : Rouge", 1994),
     #
-    ("Le scaphandre et le papillon", 2007),
+    (u"Le scaphandre et le papillon", 2007),
     #
-    ("Before Sunset", 2004),
+    (u"Before Sunset", 2004),
 ]

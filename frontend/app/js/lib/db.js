72a4ee17d2befaf9beec0703ed1504f731e6fdfe
Nikola Klaric <nikola@klaric.org>
2014-08-13 00:29:38 +0200

    Refactored addition of new movies.

--- frontend/app/js/lib/db.js
+++ frontend/app/js/lib/db.js
@@ -16,105 +16,132 @@ ka.lib.addMovie = function (movieDict) {
         return;
     }
 
-    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''), compare = ka.state.collator.compare,
-        localizedArticles = ka.lib.getLocalizedArticles();
+    var title, key, sortCriterionKey, sortedList;
 
     for (var orders = ['titleOriginal', 'titleLocalized', 'year', 'rating'], order, o = 0; order = orders[o]; o++) {
-        var field = 'by' + order[0].toUpperCase() + order.slice(1);
+        /* if (movieDict.compilation) {
+            title = movieDict.compilation;
+        } else { */
+            if (order == 'titleOriginal') {
+                title = movieDict.titleOriginal;
+            } else {
+                title = movieDict.titleLocalized;
+            }
+        /* }
+        movieDict.titleComparable = title; */
 
         if (order == 'titleOriginal' || order == 'titleLocalized') {
-            var title = movieDict[order],
-                criterion = title.replace(new RegExp('^' + localizedArticles + ' ', 'i'), '').replace('.', '').toLocaleLowerCase(),
-                key = new RegExp('^(?:' + localizedArticles + ' )?([\\S])', 'i').exec(title)[1].toLocaleUpperCase().replace(/[0-9]/, '123');
-
-            /* Fix keys that are not Latin. */
-            if (key !== '123' && /^[^A-Z]$/.test(key)) {
-                for (var index = 0; index < 26; index++) {
-                    if (compare(key, alphabet[index]) > 0) {
-                        key = alphabet[index];
-                        break;
-                    }
-                }
-            }
-
-            if (key !== '123' && /^[^A-Z]$/.test(key)) {
-                key = '?';
-            }
+            key = ka.lib._getCollatedKey(new RegExp('^(?:' + ka.lib.getLocalizedArticles() + ' )?([\\S])', 'i').exec(title)[1].toLocaleUpperCase().replace(/[0-9]/, '123'));
         } else if (order == 'year') {
-            var criterion = movieDict.titleLocalized.replace(/^the /i, '').replace('.', '').toLowerCase(),
-                key = movieDict.releaseYear;
+            key = movieDict.releaseYear;
         } else if (order == 'rating') {
-            var criterion = movieDict.titleLocalized.replace(/^the /i, '').replace('.', '').toLowerCase(),
-                rating = movieDict.rating / 10, key;
-            if (!rating) {
-                key = '?';
-            } else if (rating < 2) {
-                key = '< 2';
-            } else if (rating < 3) {
-                key = '> 2';
-            } else if (rating < 4) {
-                key = '> 3';
-            } else if (rating < 5) {
-                key = '> 4';
-            } else if (rating < 6) {
-                key = '> 5';
-            } else if (rating < 6.5) {
-                key = '> 6';
-            } else if (rating > 9) {
-                key = '> 9';
-            } else if (rating > 8.5) {
-                key = '> 8.5';
-            } else if (rating > 8) {
-                key = '> 8';
-            } else if (rating > 7.5) {
-                key = '> 7.5';
-            } else if (rating > 7) {
-                key = '> 7';
-            } else if (rating >= 6.5) {
-                key = '> 6.5';
-            }
+            key = ka.lib._keyByRating(movieDict.rating / 10);
         }
 
-        if (key in ka.data[field]) {
-            var sortedList = ka.data[field][key];
+        sortCriterionKey = 'by' + order[0].toUpperCase() + order.slice(1);
+        if (key in ka.data[sortCriterionKey]) {
+            sortedList = ka.data[sortCriterionKey][key];
         } else {
-            var sortedList = new Cortex([]);
+            /* sortedList = new Cortex([]); */
+            sortedList = [];
         }
-
-        ka.lib._insertSorted(sortedList, movieDict, criterion, field);
-        ka.data[field][key] = sortedList;
+        ka.lib._insertSorted(sortedList, movieDict, ka.lib._getComparableTitle(title), sortCriterionKey);
+        ka.data[sortCriterionKey][key] = sortedList;
     }
 
     ka.data.byUuid[movieDict.uuid] = movieDict;
-    ka.data.all.push(movieDict);
 };
 
 
-ka.lib._insertSorted  = function (sortedListRef, item, primaryCriterion, field) {
-    item[field] = primaryCriterion;
+ka.lib._insertSorted  = function (sortedListRef, movieDict, comparableTitle, sortCriterion) {
+    movieDict[sortCriterion] = comparableTitle;
 
     var compare = ka.state.collator.compare;
 
-    if (sortedListRef.count() == 0) {
-        sortedListRef.push(item);
-    } else if (sortedListRef.count() == 1) {
-        if (compare(sortedListRef[0][field].getValue(), primaryCriterion) < 0) {
-            sortedListRef.push(item);
+    if (sortedListRef.length == 0) {
+        sortedListRef.push(movieDict);
+    } else if (sortedListRef.length == 1) {
+        if (compare(sortedListRef[0][sortCriterion], comparableTitle) < 0) {
+            sortedListRef.push(movieDict);
         } else {
-            sortedListRef.unshift(item);
+            sortedListRef.unshift(movieDict);
         }
     } else {
-        var index = sortedListRef.findIndex(function (wrapperElement, index) {
-            if (index == 0 || index == sortedListRef.count() - 1) {
-                return compare(primaryCriterion, sortedListRef[index][field].getValue()) < 0;
+        var position = -1;
+        for (var index = 0; index < sortedListRef.length; index++) {
+            if (index == 0 || index == sortedListRef.length - 1) {
+                if (compare(comparableTitle, sortedListRef[index][sortCriterion]) < 0) {
+                    position = index;
+                    break;
+                }
             } else {
-                return compare(primaryCriterion, sortedListRef[index][field].getValue()) < 0 && compare(primaryCriterion, sortedListRef[index -1][field].getValue()) > 0;
+                if (compare(comparableTitle, sortedListRef[index][sortCriterion]) < 0 && compare(comparableTitle, sortedListRef[index -1][sortCriterion]) > 0) {
+                    position = index;
+                    break;
+                }
             }
-        });
-        if (index == -1) {
-            sortedListRef.push(item);
-        } else {
-            sortedListRef.insertAt(index, item);
         }
-    }    
+        if (position == -1) {
+            sortedListRef.push(movieDict);
+        } else {
+            sortedListRef.splice(position, 0, movieDict);
+        }
+    }
+};
+
+
+ka.lib._keyByRating = function (rating) {
+    if (!rating) {
+        return '?';
+    } else if (rating < 2) {
+        return '< 2';
+    } else if (rating < 3) {
+        return '> 2';
+    } else if (rating < 4) {
+        return '> 3';
+    } else if (rating < 5) {
+        return '> 4';
+    } else if (rating < 6) {
+        return '> 5';
+    } else if (rating < 6.5) {
+        return '> 6';
+    } else if (rating > 9) {
+        return '> 9';
+    } else if (rating > 8.5) {
+        return '> 8.5';
+    } else if (rating > 8) {
+        return '> 8';
+    } else if (rating > 7.5) {
+        return '> 7.5';
+    } else if (rating > 7) {
+        return '> 7';
+    } else if (rating >= 6.5) {
+        return '> 6.5';
+    }
+};
+
+
+ka.lib._getCollatedKey = function (key) {
+    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+        compare = ka.state.collator.compare;
+
+    if (key !== '123' && /^[^A-Z]$/.test(key)) {
+        for (var index = 0; index < 26; index++) {
+            if (compare(key, alphabet[index]) > 0) {
+                key = alphabet[index];
+                break;
+            }
+        }
+    }
+
+    if (key !== '123' && /^[^A-Z]$/.test(key)) {
+        key = '?';
+    }
+
+    return key;
+};
+
+
+ka.lib._getComparableTitle = function (title) {
+    return title.replace(new RegExp('^' + ka.lib.getLocalizedArticles() + ' ', 'i'), '').replace('.', '').toLocaleLowerCase();
 };

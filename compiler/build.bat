0d1d2a47bdc6f0238c8b7d49479804042fbf6277
Nikola Klaric <nikola@klaric.org>
2014-08-12 15:42:48 +0200

    Bumped version.

--- compiler/build.bat
+++ compiler/build.bat
@@ -1,5 +1,5 @@
 REM signtool signwizard
-REM http://timestamp.verisign.com/scripts/timstamp.dll
 REM http://nikola.github.io/ka-BOOM/
+REM http://timestamp.verisign.com/scripts/timstamp.dll
 del dist\ka-BOOM.exe
 pyi-build ka-BOOM.spec --distpath=dist --workpath=build --noconfirm --ascii

4cc9196005ddcf5c9d450fea3c2c71a7b70143fc
Nikola Klaric <nikola@klaric.org>
2014-09-18 23:32:05 +0200

    Added copy'n'pasteable remark.

--- compiler/build.bat
+++ compiler/build.bat
@@ -1,5 +1,6 @@
 REM https://forum.startcom.org/viewtopic.php?f=15&t=1654&st=0&sk=t&sd=a
 REM signtool signwizard
+REM ka-BOOM.exe
 REM A movie compilation and playback app for Windows.
 REM https://github.com/nikola/ka-BOOM
 REM http://timestamp.verisign.com/scripts/timstamp.dll

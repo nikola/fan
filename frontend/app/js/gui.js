bbe8d25f5eb6776d4b8dd1e64d99bbb2aaad3bc5
Nikola Klaric <nikola@klaric.org>
2014-10-15 20:37:51 +0200

    Renamed element.

--- frontend/app/js/gui.js
+++ frontend/app/js/gui.js
@@ -199,7 +199,7 @@ window.onpopstate = function () {
 
 $(document).ready(function () {
     ka.state.maxConfigButton = $('#boom-menu .boom-button').length;
-    ka.state.canvasContext = $('#boom-image-color-canvas').get(0).getContext('2d');
+    ka.state.canvasContext = $('#boom-image-sampler-canvas').get(0).getContext('2d');
     /* console.log(ka.state.canvasContext.webkitBackingStorePixelRatio == 1);
     console.log(window.devicePixelRatio == 1); */
 
